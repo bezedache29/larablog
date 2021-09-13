3d8c59e11c83bba2e910086c21af49482cd83e8a
Christophe Salou <bezedache29@gmail.com>
2021-09-13 16:26:29 +0200

    Intégration du theme

---
+++ resources/js/text.js
@@ -0,0 +1,9 @@
+const variable = 1;
+
+if (variable != 3) {
+  console.log('pas bon')
+} else {
+  console.log('Ok')
+}
+
+// Ici un bla bla
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+// Ici un commentaire
+Route::resource('posts', PostController::class)->except('index');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
